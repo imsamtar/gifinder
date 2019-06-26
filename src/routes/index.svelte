9bf815cdc666cfdd1657c59f14fd4af380b7fac3
SAMEER TARIQ <imsamtar@gmail.com>
2019-06-26 22:35:51 +0500

    Basic functionality Working...

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,78 @@
+<script>
+	import {onMount} from 'svelte';
+	onMount(() => {
+		document.getElementById('search').focus();
+	})
+	let gifs = [];
+	let keyword='';
+	let loading = false;
+	function getGifs(event){
+		loading = true;
+		event.preventDefault();
+		fetch(`https://api.giphy.com/v1/gifs/search?api_key=O0PIjSQKNXlhQl4ro8ngi3paQujecP6C&q=${keyword}&limit=25&offset=0&rating=G&lang=en`)
+		.then(res => res.json())
+		.then(json => gifs = json.data);
+		loading = false;
+	}
+
+</script>
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
+form {
+	display: flex;
+	margin: 10px 0px;
+}
+input {
+	padding: 8px 10px;
+	border-radius: 5px;
+	border: 1px solid gray;
+	box-sizing: border-box;
+	width: 100%;
+	outline: 0;
+}
+input[type="submit"]{
+	width: 70px;
+	color: white;
+	background: black;
+	border: 0;
+	border-radius: 0;
+}
+.images {
+	column-count: 4;
+}
+img {
+	width: 100%;
+	height: auto;
+}
+@media screen and (max-width: 1024px){
+	.images {
+		column-count: 3;
 	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
+}
+@media screen and (max-width: 700px){
+	.images {
+		column-count: 2;
 	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
+}
+@media screen and (max-width: 400px){
+	form {display: block}
+	input {
 		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
+		display: block;
 	}
-
-	p {
-		margin: 1em auto;
+	input[type="submit"]{
+		width: 100%;
 	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
+	.images {
+		column-count: 1;
 	}
+}
 </style>
-
-<svelte:head>
-	<title>Sapper project template</title>
-</svelte:head>
-
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<form on:submit={getGifs}>
+	<input id="search" type="text" style="index: 0" placeholder="Search your favourite gif images" bind:value={keyword} autofocus disabled={loading}>
+	<input type="submit" value="Search" disabled={loading}/>
+</form>
+<div class="images">
+	{#each gifs as gif}
+		<img autoplay src={gif.images.fixed_height.url} alt="tag" width="200px">
+	{/each}
+</div>
